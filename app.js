78a7bf9be7b95fdefbd471f24514dec0eb86054e
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2021-06-10 18:29:31 -0400

    Add functionality to trash button

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
-
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -39,4 +39,19 @@ function addTodo(event){
 
     //CLEAR TODO INPUT VALUE
     todoInput.value = "";
+}
+
+function deleteCheck(event){
+    const item = event.target;
+
+    //DELETE TODO
+    if(item.classList[0] ==='trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    //CHECKMARK
+    if(item.classList[0] ==='complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
 }
